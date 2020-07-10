23e4c91445c0b41ce0957e5c0c28439660556171
NotBlankyu <dragonhuntermlw@gmail.com>
2020-07-10 11:21:36 +0100

    Added invite command.

---
+++ commands/invite.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+
+module.exports.run = async (client, message, args) => {
+    const Embed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Invite me!')
+	.setURL('https://discord.com/api/oauth2/authorize?client_id=705053632961446008&permissions=3196928&scope=bot')
+	.setImage('https://i.imgur.com/nx7Olli.jpg','https://discord.com/api/oauth2/authorize?client_id=705053632961446008&permissions=3196928&scope=bot')
+	.setTimestamp()
+	.setFooter('Arya', 'https://i.imgur.com/nx7Olli.jpg');
+
+	message.channel.send(Embed);
+  }
+
+

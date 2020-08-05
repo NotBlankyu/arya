1370f32de72b565990dd4d063dd6508648df8573
NotBlankyu <dragonhuntermlw@gmail.com>
2020-08-05 12:24:01 +0100

    changed invite command

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,6 @@ module.exports.run = async (client, message, args) => {
 	.setColor('#0099ff')
 	.setTitle('Invite me!')
 	.setURL('https://discord.com/api/oauth2/authorize?client_id=705053632961446008&permissions=3196928&scope=bot')
-	.setImage('https://i.imgur.com/nx7Olli.jpg','https://discord.com/api/oauth2/authorize?client_id=705053632961446008&permissions=3196928&scope=bot')
 	.setTimestamp()
 	.setFooter('Arya', 'https://i.imgur.com/nx7Olli.jpg');
 

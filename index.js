5ba9afd8fc2ae7088ef91feb0d8adbc0aa882979
NotBlankyu <66680364+NotBlankyu@users.noreply.github.com>
2020-09-23 13:55:04 +0100

    inicial commit

---
+++ index.js
@@ -0,0 +1,253 @@
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const ytdl = require("ytdl-core");
+const ytsr = require('ytsr')
+const ytpl = require('ytpl')
+
+const client = new Discord.Client();
+
+const queue = new Map();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue, client);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}queue`)) {
+    queueList(message.guild,message)
+  } else {
+    message.channel.send("You need to enter a valid command!");
+  }
+});
+
+async function execute(message, serverQueue, client) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  let songInfo = 0
+  let playlistSongs = []
+  let song
+  
+  if(!ytdl.validateURL(args[1])){ //check if is a link, if not execute ytsr or ytpl
+    try {
+      playlistID = await ytpl.getPlaylistID(args[1])
+       await ytpl(playlistID).then(playlist => {
+          for (let i = 0; i < playlist.total_items; i++) {
+            playlistSongs.push({
+              title: playlist.items[i].title,
+              url: playlist.items[i].url_simple
+            })
+          }
+        }).catch(err => {
+            console.error(err);
+        });
+    } catch (err) {
+      searchArgs = args.slice(0).join(' ')
+      searchResult = await ytsr(searchArgs);
+      musicLink = searchResult.items[0].link
+      songInfo = await ytdl.getInfo(musicLink);
+      song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url
+      };
+    }
+    
+  }else{                          //gets the info from the link if it is a yt link
+    songInfo = await ytdl.getInfo(args[1]);
+    song = {
+      title: songInfo.videoDetails.title,
+      url: songInfo.videoDetails.video_url
+    };
+  }
+  
+  
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      msgToDel: null,
+      connection: null,
+      queueEmbeds: [],
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    if(await playlistSongs[0]){
+      for (let i = 0; i < playlistSongs.length; i++) {
+        queueContruct.songs.push(playlistSongs[i]);
+      }
+    }else{
+      queueContruct.songs.push(song);
+    }
+    
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0], message, client);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    if(await playlistSongs[0]){
+      for (let i = 0; i < playlistSongs.length; i++) {
+        serverQueue.songs.push(playlistSongs[i]);
+      }
+      return message.channel.send(new Discord.MessageEmbed().setDescription(`The **[Playlist](${args[1]})** has been added to the queue!`));
+    }else{
+      serverQueue.songs.push(song);
+      return message.channel.send(new Discord.MessageEmbed().setDescription(`**[${song.title}](${song.url})** has been added to the queue!`));
+    }
+    
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+async function play(guild, song, message) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  if(serverQueue.msgToDel){
+    serverQueue.textChannel.messages.fetch(serverQueue.msgToDel).then(message => message.delete())
+  }
+  msg = await serverQueue.textChannel.send( new Discord.MessageEmbed().setDescription(`Start playing: **[${song.title}](${song.url})**`) );
+  serverQueue.msgToDel = msg.id
+}
+
+function queueList(guild,message) {
+  const serverQueue = queue.get(guild.id);
+  if(!serverQueue.songs[0]){
+    return message.channel.send("A queue doesn't exist in the moment.")
+  }
+  var queueText = ''
+  serverQueue.queueEmbeds = []
+  for(var i = 0; i < serverQueue.songs.length; ++i){
+    queueText += `${i + 1}. [${serverQueue.songs[i].title}](${serverQueue.songs[i].url})\n`;
+  }
+  const [first, ...rest] = Discord.Util.splitMessage(queueText, { maxLength: 2048 })
+          
+  const embed2 = new Discord.MessageEmbed()
+  .setTitle('Music Queue')
+  .setDescription(`${first}`)
+  serverQueue.queueEmbeds.push(embed2)
+if (!rest.length) {
+  // Send just the embed with the first element from the array
+  return message.channel.send(embed2)
+}
+let i2 = 0;
+
+for (const text of rest) {
+  // Add new description to the base embed
+  embed3 = new Discord.MessageEmbed()
+  .setTitle('Music Queue')
+  .setDescription(`${text}`)
+  serverQueue.queueEmbeds.push(embed3)
+}
+const filter1 = (reaction, user) => {
+  return reaction.emoji.name === '➡️' && user.id === message.author.id;
+};
+const filter2 = (reaction, user) => {
+  return reaction.emoji.name === '⬅️' && user.id === message.author.id;
+}; 
+
+message.channel.send(serverQueue.queueEmbeds[0]).then(msg => {
+msg.react('➡️')
+const collector1 = msg.createReactionCollector(filter1, { time: 30000 });
+collector1.on('collect', (reaction, user) => {
+  i2 += 1
+  msg.edit(serverQueue.queueEmbeds[i2])
+  msg.reactions.removeAll();
+  if(i2+1 == serverQueue.queueEmbeds.length){
+    msg.react('⬅️')
+  }else{
+    msg.react('⬅️')
+    msg.react('➡️')
+  }
+  
+});
+const collector2 = msg.createReactionCollector(filter2, { time: 30000 });
+collector2.on('collect', (reaction, user) => {
+  i2 -= 1
+  msg.edit(serverQueue.queueEmbeds[i2])
+  msg.reactions.removeAll();
+  if(i2==0){
+    msg.react('➡️')
+  }else{
+  msg.react('⬅️')
+  msg.react('➡️')
+}
+});
+});
+}
+
+client.login(token);
+
